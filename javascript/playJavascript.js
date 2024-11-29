fcc24e696846f9ce5a4a282e478503e066ab4729
meshboy88 <meshbeats88@gmail.com>
2024-11-29 20:19:14 -0300

    first commit

---
+++ javascript/playJavascript.js
@@ -0,0 +1,5 @@
+function runJS(name) {
+    return "JavaScript: Hello " + name;
+}
+
+console.log(runJS("Lucas"));
---
+++ typescript/playJavascript.ts
@@ -0,0 +1,5 @@
+function runTS(name) {
+    return "TypeScript: Hello " + name;
+}
+
+console.log(runTS("Lucas"));
