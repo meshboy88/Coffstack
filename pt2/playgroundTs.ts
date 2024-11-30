d2d6d8761e8b5624d0e825a0fdad28e194ac976d
meshboy88 <meshbeats88@gmail.com>
2024-11-30 04:37:27 -0300

    key of type operator

--- pt2/playgroundTs.ts
+++ pt2/playgroundTs.ts
@@ -72,3 +72,70 @@ const numberResult = identity(42); // Aqui, o TypeScript infere que T é 'number
 console.log(numberResult); // 42
 
 */
+
+//CONSTRAINT em Generics
+
+// constrain
+// value user: User
+// Aqui eu to passando um tipo genérico pra função, mas to falando que ele tem obrigatoriamente que ter um user do tipo User
+
+interface WithUser {
+    user: User;
+}
+
+function getAuthorName<Type extends WithUser>(value: Type): string {
+    return value.user.name;
+}
+
+const name = getAuthorName(post);
+
+
+//KEYOF TYPE OPERATOR
+/*As propriedades de uma variável são também chamadas de chaves
+no caso do lucas2, seria o name, userName e email
+Ao passar no segundo argumento da função a chave como keyof User, por exemplo, eu estou dizendo que
+eu quero o valor de alguma propriedade daquele tipo, e ai quando eu chamo a função, ao chamar aquela chave, ja vai me retornar o valor da chave
+Como estou usando generics, significa que eu posso chamar a função get property com o objeto comment
+e como no outro arquivo o objeto comment ja ta tipado como PostComment, eu posso chamar no segundo argumento da função
+qualquer chave desse objeto e ele vai me retornar o valor correspondente.  
+
+export const comment: PostComment = {
+    content: "Conteúdo do comentário",
+    likes: 5,
+    user: vitor,
+};
+
+export interface PostComment {
+    content: string;
+    likes: number;
+    user: User;
+}
+
+*/
+
+
+export const lucas2: User = {
+    name: "Lucas",
+    userName: "lucasgar6",
+    email: "lucas@coffstack.com",
+};
+
+function getProperty<Type>(value: Type, key: keyof Type) {
+    return value[key];
+}
+
+const valueX = getProperty(comment, "content");
+console.log(value);
+
+//Ou seja, no console vai aparecer "conteúdo do comentário", 
+//que é o valor da chave content no objeto comment
+//Como to passando o tipo generic, sei que a chave tem o mesmo tipo do valor, que no caso seria PostComment
+//O tipo do valor retornado é inferido automaticamente com base na chave. 
+//No caso de "content", o valor será do tipo string (porque content é string em PostComment). 
+// Se a chave fosse "likes", o valor seria do tipo number.
+
+
+// Resumo do que acontece:
+// - Você passa o objeto `comment`
+// - Especifica a chave "content"
+// - A função retorna o valor correspondente: "Conteúdo do comentário"
