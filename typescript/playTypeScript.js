a514dd3921c95ec6ac2cdede82ccca5576e6e267
meshboy88 <meshbeats88@gmail.com>
2024-11-29 23:19:01 -0300

    adjustments

--- typescript/playTypeScript.js
+++ typescript/playTypeScript.js
@@ -3,3 +3,23 @@ function runTS(name) {
     return "TypeScript: Hello " + name;
 }
 console.log(runTS("Lucas"));
+
+//tipagem dinâmica
+let message = "Fala bb"
+message = 6
+message= "Nope"
+console.log(message.toUpperCase())
+
+function flipCoin() {
+    return Math.random() < 0.5 ? "cara" : "coroa";
+}
+
+const result = flipCoin()
+
+if (result === "caara") {
+    console.log("Você ganhou!")
+} else {
+    console.log("Você perdeu!")
+}
+
+// Aqui o código vai rodar mas sempre vai dar perdeu
--- typescript/playTypeScript.ts
+++ typescript/playTypeScript.ts
@@ -6,3 +6,23 @@ function runTS(name: string): string {
 }
 
 console.log(runTS("Lucas"));
+
+//tipagem dinâmica(js) vs tipagem estática(ts)
+//basicamente significa que eu posso mudar o tipo no js (ex: string => number), coisa que no ts não posso
+
+//Inferência de tipo
+
+function flipCoin() {
+    return Math.random() < 0.5 ? "cara" : "coroa";
+}
+
+const result = flipCoin()
+
+if (result === "cara") { //antes tava "caara"
+    console.log("Você ganhou!")
+} else {
+    console.log("Você perdeu!")
+}
+
+//Aqui o código nem vai rodar por conta da inferência de tipo do ts
+//Ele percebe que a comparação não tem overlap (sobreposição), porque caara não é um retorno possível
